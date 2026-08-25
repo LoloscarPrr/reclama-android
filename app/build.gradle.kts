874d63cc612fe27291250372a178df4708f4a2a8
LoloscarPrr <oscar.ignacio.urrutia@gmail.com>
2026-08-25 11:21:17 -0400

    chore(release): bump app to v0.2.0-alpha

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "cl.reclama.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0-alpha"
+        versionCode = 2
+        versionName = "0.2.0-alpha"
     }
 
     buildFeatures { compose = true }
